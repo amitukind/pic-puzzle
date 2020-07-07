ec0d73b2fc1837632c079753796bc64c73237dbe
Amit Verma <amitverma.ukind@gmail.com>
2020-07-07 16:11:48 +0530

    Scene Setup

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var config = {
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
+  width: 540,
+  height: 960,
   scene: {
     preload: preload,
     create: create,
@@ -12,9 +12,21 @@ var config = {
 var game = new Phaser.Game(config);
 
 function preload() {
-  console.log("Preload");
+  this.load.image('tiger', 'images/Tiger.png');
+  this.load.image('bl', 'images/reveal_bottomleft.png');
+  this.load.image('br', 'images/reveal_bottomright.png');
+  this.load.image('tl', 'images/reveal_topleft.png');
+  this.load.image('tr', 'images/reveal_topright.png');
 }
 
-function create() {}
+function create() {
+  this.cameras.main.backgroundColor.setTo(255,255,255);
+  this.add.image(270, 480, 'tiger');
+  this.add.image(270-50, 480+50, 'tl');
+  this.add.image(270+50, 480+50, 'tr');
+  this.add.image(270+50, 480-50, 'bl');
+  this.add.image(270-50, 480-50, 'br');
 
-function update() {}
+}
+
+function update() { }
