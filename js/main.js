6b7926eb2b85ed9668c6946f7bd01832bef7c3f7
Amit Verma <amitverma.ukind@gmail.com>
2020-07-07 08:58:36 +0530

    Phaser3 Setup 1.0

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  scene: {
+    preload: preload,
+    create: create,
+    update: update,
+  },
+};
+
+var game = new Phaser.Game(config);
+
+function preload() {
+  console.log("Preload");
+}
+
+function create() {}
+
+function update() {}
